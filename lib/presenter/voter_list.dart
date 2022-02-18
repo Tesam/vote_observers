d5abdc80d4837c4c216c254892edaf6c340384a0
tesam <mtsamudiog@gmail.com>
2022-02-18 18:22:13 -0300

    first commit

---
+++ lib/presenter/voter_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VoterList extends StatelessWidget {
+  const VoterList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text("Voter list"),
+    );
+  }
+}
