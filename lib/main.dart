4709366703dc8b57693d6af6b0efe5f5a8511fe6
tesam <mtsamudiog@gmail.com>
2022-02-18 23:36:35 -0300

    configure firebase project for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:vote_observers/presenter/voter_list.dart';
 
-void main() {
+
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
