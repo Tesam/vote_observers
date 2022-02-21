c153959c7f56e449c3bd10bfb3b8b7fed67a979f
tesam <mtsamudiog@gmail.com>
2022-02-21 08:51:28 -0300

    style(my_theme.dart): add color palette

--- lib/presenter/my_theme.dart
+++ lib/presenter/my_theme.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MyTheme {
-  static const darkColor = Color(0xFF1D8233);
-  static const grayBackground = Color(0xFFF2F2F2);
+  static const darkGreen = Color(0xFF1D8233);
+  static const grayBackground = Color(0xFFE5E5E5);
   static const primaryColor = Color(0xFFBCEEA5);
-  static const grayText = Color(0xFF828282);
+  static const gray3Text = Color(0xFF828282);
+  static const gray2Text = Color(0xFF4F4F4F);
+  static const background = Color(0xFFF6F6F6);
 }
