f8ab410989745abf500031bffd6e72e6870fecc4
tesam <mtsamudiog@gmail.com>
2022-02-21 22:04:22 -0300

    feat(my_theme.dart): add new colors

--- lib/presenter/my_theme.dart
+++ lib/presenter/my_theme.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class MyTheme {
   static const darkGreen = Color(0xFF1D8233);
-  static const grayBackground = Color(0xFFE5E5E5);
   static const primaryColor = Color(0xFFBCEEA5);
-  static const gray3Text = Color(0xFF828282);
+  static const grayBackground = Color(0xFFE5E5E5);
   static const gray2Text = Color(0xFF4F4F4F);
+  static const gray3Text = Color(0xFF828282);
+  static const gray4Text = Color(0xFFBDBDBD);
   static const background = Color(0xFFF6F6F6);
 }
