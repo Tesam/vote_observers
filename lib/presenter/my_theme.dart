b49cfb4f96bc9b9ce5fb624f4f21e0eb3ded1b52
tesam <mtsamudiog@gmail.com>
2022-02-23 21:12:41 -0300

    feat(my_theme.dart): add red color

--- lib/presenter/my_theme.dart
+++ lib/presenter/my_theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class MyTheme {
   static const darkGreen = Color(0xFF1D8233);
   static const primaryColor = Color(0xFFBCEEA5);
+  static const redColor = Color(0xFFFF8C8C);
   static const grayBackground = Color(0xFFE5E5E5);
   static const gray2Text = Color(0xFF4F4F4F);
   static const gray3Text = Color(0xFF828282);
