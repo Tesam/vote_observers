9dda4e5656a431b291515e78f3694d5cd04033ec
tesam <mtsamudiog@gmail.com>
2022-02-26 09:35:58 -0300

    feat(partners_table.dart): get partners from database

---
+++ lib/data/partners_table.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:vote_observers/domain/models/partner.dart';
+
+class PartnersTable {
+  final CollectionReference partnersRef =
+      FirebaseFirestore.instance.collection("partners").withConverter<Partner>(
+            fromFirestore: (snapshot, _) => Partner.fromJson(snapshot.data()!),
+            toFirestore: (partner, _) => partner.toJson(),
+          );
+
+  Future<Partner> getPartner({required String partnerID}) async {
+    final Partner partner =
+        (await partnersRef.doc(partnerID).get()).data()! as Partner;
+
+    return partner;
+  }
+
+  Future<List<Partner>> getPartners({required List<dynamic> partnerIDs}) async {
+    final partners = partnerIDs.map((partnerID) async => await getPartner(partnerID: partnerID));
+
+    return Future.wait(partners);
+  }
+}
