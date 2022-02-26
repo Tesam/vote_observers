4c58e9b0cf97d3ff0cab72320a1a14da1db97c23
tesam <mtsamudiog@gmail.com>
2022-02-26 11:07:39 -0300

    feat(add_operator.dart): create an operator

--- lib/presenter/add_operator.dart
+++ lib/presenter/add_operator.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:vote_observers/data/operators_table.dart';
 import 'package:vote_observers/data/partners_table.dart';
+import 'package:vote_observers/domain/models/operator.dart';
 import 'package:vote_observers/domain/models/partner.dart';
 import 'package:vote_observers/presenter/my_theme.dart';
 import 'package:vote_observers/presenter/widgets/my_button.dart';
@@ -8,10 +10,13 @@ import 'package:vote_observers/presenter/widgets/my_text_field.dart';
 class AddOperator extends StatelessWidget {
   const AddOperator({Key? key}) : super(key: key);
   static PartnersTable partnersTable = PartnersTable();
+  static OperatorsTable operatorsTable = OperatorsTable();
 
   static TextEditingController searchController = TextEditingController();
   static TextEditingController nameController = TextEditingController();
   static TextEditingController phoneController = TextEditingController();
+
+  static late Partner operator;
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +43,8 @@ class AddOperator extends StatelessWidget {
                       Expanded(
                         child: MyTextField(
                           hintText: "Nro de socio del operador",
-                          textEditingController: searchController,),
+                          textEditingController: searchController,
+                        ),
                       ),
                       Container(
                         margin: const EdgeInsets.only(left: 10.0),
@@ -46,8 +52,9 @@ class AddOperator extends StatelessWidget {
                           backgroundColor: MyTheme.primaryColor,
                           child: IconButton(
                             onPressed: () async {
-                              final Partner partner = await partnersTable.getPartner(partnerID:searchController.text);
-                              nameController.text = partner.name;
+                              operator = await partnersTable.getPartner(
+                                  partnerID: searchController.text);
+                              nameController.text = operator.name;
                             },
                             icon: const Icon(
                               Icons.search,
@@ -62,16 +69,56 @@ class AddOperator extends StatelessWidget {
                   const SizedBox(
                     height: 20.0,
                   ),
-                  MyTextField(hintText: "Nombre",
-                      textFieldStatus: TextFieldStatus.disabled, textEditingController: nameController,),
+                  MyTextField(
+                    hintText: "Nombre",
+                    textFieldStatus: TextFieldStatus.disabled,
+                    textEditingController: nameController,
+                  ),
                   const SizedBox(
                     height: 10.0,
                   ),
-                  MyTextField(hintText: "Celular", textEditingController: phoneController,)
+                  MyTextField(
+                    hintText: "Celular",
+                    textEditingController: phoneController,
+                  )
                 ],
               ),
             ),
-            myButton(title: "Crear operador"),
+            MyButton(
+                title: "Crear operador",
+                onPressed: () async {
+                  bool _isAdded = await operatorsTable.createOperator(
+                      operator: Operator(
+                          name: operator.name,
+                          phone: phoneController.text,
+                          identification: operator.identification,
+                          assignedPartners: []),
+                      operatorID: searchController.text);
+
+                  (_isAdded)
+                      ? ScaffoldMessenger.of(context)
+                          .showSnackBar(const SnackBar(
+                          content: Text(
+                            'El Operador se agregó correctamente',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.black, fontSize: 16),
+                          ),
+                          duration: Duration(seconds: 3),
+                          backgroundColor: MyTheme.primaryColor,
+                          padding: EdgeInsets.symmetric(vertical: 40),
+                        ))
+                      : ScaffoldMessenger.of(context)
+                      .showSnackBar(const SnackBar(
+                    content: Text(
+                      'Error al intentar agregar operador',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                    duration: Duration(seconds: 3),
+                    backgroundColor: MyTheme.redColor,
+                    padding: EdgeInsets.symmetric(vertical: 40),
+                  ));
+                }),
           ],
         ),
       ),
