6af8f4d844a2228be8e26377c79204eb350c74d2
Techi Samudio <mtsamudiog@gmail.com>
2022-08-01 13:20:13 -0400

    UI(home test): create a home UI for test purpose

---
+++ lib/src/presenter/home/home_test.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:vote_observers/src/presenter/widgets/csm_button.dart';
+
+class HomeTest extends StatelessWidget {
+  const HomeTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CSMButton(buttonTitle: "CERRAR SESION", onPressed: () => FirebaseAuth.instance.signOut(),),
+      ),
+    );
+  }
+}
