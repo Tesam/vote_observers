bbc8d90a25d89c10b6c1c97bf2c839d443673f33
Techi Samudio <mtsamudiog@gmail.com>
2022-08-02 10:53:59 -0400

    feat(loading screen): create a loading screen

---
+++ lib/src/presenter/widgets/csm_loading_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:vote_observers/presenter/my_theme.dart';
+
+class CSMLoadingScreen extends StatelessWidget {
+  const CSMLoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: MyTheme.kBackground,
+      body: Center(
+        child: CircularProgressIndicator(color: MyTheme.kPrimaryColor,),
+      ),
+    );
+  }
+}
